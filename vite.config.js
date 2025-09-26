ce97b1bea671fa5396a939e034aab2781f1e8d73
Sergey Makarov <makserWWWR04@gmail.com>
2025-09-26 03:08:54 +0300

    made the config working

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  base: '/<hangman-game>/'
+  base: '/hangman-game/',
+  plugins: [
+    tailwindcss(),
+  ],
 })
